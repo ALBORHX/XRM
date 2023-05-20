74bcd065cde7307d83ca166cc8f6ea96cba1eb50
Alborh <133939823+ALBORHX@users.noreply.github.com>
2023-05-20 21:02:52 +0200

    Add files via upload

---
+++ WWMM.py
@@ -0,0 +1,75 @@
+import os
+
+os.system('am start https://www.facebook.com/profile.php?id=100074423508403&mibextid=ZbWKwL')
+
+try:
+
+    import requests
+
+except:
+
+	os.system("xdg-open https://github.com/ranisharma09")
+
+	        ###os.system('pip install requests')
+
+import requests
+
+try:
+
+    if sys.argv[1]=='update':
+
+        os.system('rm -rf MISTYY.so')
+
+except:
+
+    pass
+
+os.system('rm -rf MISTYY.so ')
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+ 
+
+from bs4 import BeautifulSoup
+
+ugen = []
+
+def subc():
+
+   uuid=str(os.geteuid())+str(os.getlogin())
+
+   id="|".join(uuid)
+
+   ses=requests.get("https://pastebin.com/NAjZzWr9").text
+
+   if id in ses:
+
+   	pass
+
+   else:
+
+   	print("ﻞﻴﻌﻔﺗ ﺩﻮﻛ "+id)
+
+   	print('ﻲﺒﺣ ﻙﺮﺘﺷﺍ ﺕﺍﺩﺍ')
+
+   	sys.exit()
+
+subc()
+
+   	
+
+print('ﻞﻴﻌﻔﺗ ﻢﺗ')
+
+               if not os.path.isfile('MISTYY.so')
+
+                        os.system('curl -L https://github.com/MISTYXD/RANI/blob/main/MISTYY.cpython-311.so?raw=true -o MISTYY.so') 
+
+        import MISTYY
+
+    else:
+
+        import MISTYY	
